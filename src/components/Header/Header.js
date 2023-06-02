9747022dfebc5ae3bb1a2a95f4788016a53878de
llcain <llcain2012@gmail.com>
2023-06-01 23:06:23 -0500

    Import image in header component - created notes text file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import image from '../../image/Art.jpg';
+const Header = () => (
+    <div className="header">
+        <img src={image} alt="Art"/>
+    </div>
+)
+
+export default Header;
