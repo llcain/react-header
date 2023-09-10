f29bee01ed29d768d92f3d3bf2f96ebc515b64fb
llcain <llcain2012@gmail.com>
2023-09-10 15:13:47 -0500

    Added Row react bootstrap component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Row } from 'react-bootstrap';
+
 const Home = () => {
     return (
-        <h1>Home</h1>
-    )
+        <div style={{ display: 'block', 
+                  width: 700, padding: 30 }}>
+      <h4>React-Bootstrap Row Component</h4>
+      <Row style={{
+        backgroundColor: 'red',
+      }}>
+        Sample First Row
+      </Row>
+      <Row style={{
+        backgroundColor: 'yellow',
+      }}>
+        Sample Second Row
+      </Row>
+      <Row style={{
+        backgroundColor: 'green',
+      }}>
+        Sample Third Row
+      </Row>
+    </div>
+    );
 }
     
 export default Home;
