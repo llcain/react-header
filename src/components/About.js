b55da1bd7d5d3fd31631bb1893a313fa5b866694
llcain <llcain2012@gmail.com>
2023-09-10 19:05:59 -0500

    Styled about pages - added images

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,18 @@
+import React from "react";
+import PhotoIMG1 from '../image/photoIMG1.jpg';
+
 const About = () => {
     return (
-        <h1>About Page</h1>
+        <section className="about-section">
+            <div className="row">
+                <div className="about-col">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, laudantium nostrum ut suscipit ullam illo earum aut veritatis. Rem atque obcaecati dicta quam non consectetur, impedit totam similique quasi nobis!</p>
+                </div>
+                <div className="about-col">
+                    <img className="about-img" src={PhotoIMG1} alt="Clouds" />
+                </div>
+            </div>
+        </section>
     )
 };
     
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div style={{ display: 'block', 
-                  width: 700, padding: 30 }}>
-      <h4>React-Bootstrap Row Component</h4>
-      <Row style={{
-        backgroundColor: 'red',
-      }}>
-        Sample First Row
+      <div style={{ display: 'block', width: 960, padding: 30 }}>
+      <h4>React-Bootstrap Col Component</h4>
+      <Row>
+        <Col style={{
+          backgroundColor: 'red',
+        }}>
+          Sample First Col
+      </Col>
+        <Col style={{
+          backgroundColor: 'yellow',
+        }}>
+          Sample Second Col
+      </Col>
+       
       </Row>
-      <Row style={{
-        backgroundColor: 'yellow',
-      }}>
-        Sample Second Row
+      <Row>
+        <Col style={{ backgroundColor: 'purple'}}>
+          Column
+        </Col>
+        <Col style={{ backgroundColor: 'grey'}}>
+          Column
+        </Col>
       </Row>
-      <Row style={{
-        backgroundColor: 'green',
-      }}>
-        Sample Third Row
+      <Row>
+      <Col style={{
+          backgroundColor: 'green',
+        }}>
+          Sample Third Col
+      </Col>
+      <Col style={{
+          backgroundColor: 'blue',
+        }}>
+          Sample Third Col
+      </Col>
       </Row>
     </div>
     );
