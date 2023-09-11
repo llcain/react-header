fe5a050e67da51d32f0f0c00817db60d6a73c0ab
llcain <llcain2012@gmail.com>
2023-09-10 22:45:38 -0500

    Updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -12,6 +12,22 @@ const About = () => {
                     <img className="about-img" src={PhotoIMG1} alt="Clouds" />
                 </div>
             </div>
+            <div className="row">
+                <div className="about-col">
+                    <img className="about-img" src={PhotoIMG1} alt="Clouds" />
+                </div>
+                <div className="about-col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, laudantium nostrum ut suscipit ullam illo earum aut veritatis. Rem atque obcaecati dicta quam non consectetur, impedit totam similique quasi nobis!</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="about-col">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, laudantium nostrum ut suscipit ullam illo earum aut veritatis. Rem atque obcaecati dicta quam non consectetur, impedit totam similique quasi nobis!</p>
+                </div>
+                <div className="about-col">
+                    <img className="about-img" src={PhotoIMG1} alt="Clouds" />
+                </div>
+            </div>
         </section>
     )
 };
