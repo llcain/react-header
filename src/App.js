487e641c87ac2b0e23594e951b6a928a8e2ce946
llcain <llcain2012@gmail.com>
2024-03-10 21:32:51 -0500

    Add What we do page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import WhatWeDo from './components/WhatWeDo';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home/>} />
           <Route path="/about" exact element={<About/>} />
+          <Route path="/whatwedo" exact element={<WhatWeDo/>} />
           <Route path="/contact" exact element={<Contact/>} />
         </Routes>
         </Router>      
