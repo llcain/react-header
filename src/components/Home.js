bc67ff9052c78808a90f6ae13067b130263e1534
llcain <llcain2012@gmail.com>
2024-03-16 00:00:49 -0500

    Changed the background color of the header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Col } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import { Col } from 'react-bootstrap';
+// import { Row } from 'react-bootstrap';
 import Layout from './Layout/Layout';
+import bindery from '../image/bindery.jpg';
 
 const Home = () => {
     return (
       <Layout>
-      <div style={{ display: 'block', width: 960, padding: 30 }}>
-      <h4>React-Bootstrap Col Component</h4>
-      <Row>
-        <Col style={{
-          backgroundColor: 'red',
-        }}>
-          Sample First Col
-      </Col>
-        <Col style={{
-          backgroundColor: 'yellow',
-        }}>
-          Sample Second Col
-      </Col>
-       
-      </Row>
-      <Row>
-        <Col style={{ backgroundColor: 'purple'}}>
-          Column
-        </Col>
-        <Col style={{ backgroundColor: 'grey'}}>
-          Column
-        </Col>
-      </Row>
-      <Row>
-      <Col style={{
-          backgroundColor: 'green',
-        }}>
-          Sample Third Col
-      </Col>
-      <Col style={{
-          backgroundColor: 'blue',
-        }}>
-          Sample Third Col
-      </Col>
-      </Row>
-    </div>
+        <div className='container'>
+          <div className="row">
+            <div className='column'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio earum qui officiis quo quia facere, sint facilis quaerat ratione inventore impedit culpa sequi, rerum consequatur! Facere quibusdam minima error?</p>
+            </div>
+            <div className='column'>
+              <img src={bindery} alt='bindery photo' />
+
+            </div>
+          </div>
+        </div>
     </Layout>
     );
 }
