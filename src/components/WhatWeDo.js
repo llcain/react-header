f7ea7990d05598a4224b50035db8669bf1839a8e
llcain <llcain2012@gmail.com>
2024-05-02 15:55:12 -0500

    Updated content on what we do page

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -1,9 +1,40 @@
 import Layout from "./Layout/Layout";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import press from '../image/press.jpg';
+import dgmPress from '../image/dgm-press.jpg';
+import gmaInserter from '../image/gma-inserter.jpg';
 
 const WhatWeDo = () => {
     return (
         <Layout>
-        <h1>What We Do</h1>
+        <h2>What We Do</h2>
+        <Container>
+            <Row>
+                <Col>
+                    <div>
+                        <img src={press} alt="Universal press"/>
+                        <h3>Universal Press</h3>
+                        <p>We have 2 universal presses that can print up to 45,000 broadsheet papers and hour.</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <img src={dgmPress} alt="DGM press"/>
+                        <h3>DGM Press</h3>
+                        <p>Our DGM press can print 20,000 copies an hour.</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <img src={gmaInserter} alt="GMS Inserter"/>
+                        <h3>GMA Inserter</h3>
+                        <p>We have 2 GMA inserters that have 12 and 10 pockets for inserts that can run about 10,000 copies an hour.</p>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
         </Layout>
     )
 }
