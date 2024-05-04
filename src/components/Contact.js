ef5be8d77841fab8bdd2ef21b9ba416a911d72fd
llcain <llcain2012@gmail.com>
2024-05-04 13:09:36 -0500

    Added contact information on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,28 @@
 import Layout from "./Layout/Layout";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Contact = () => {
    return (
       <Layout>
-          <h1>Contact Page</h1>
+          <Container>
+            <Row>
+               <Col>
+                  <div>
+                     <h2>Hours</h2>
+                     <p>Monday - Friday</p>
+                     <p>9 am to 5 pm</p>
+                  </div>
+               </Col>
+               <Col>
+                  <div>
+                     <h2>Call Us</h2>
+                     <p>Local: 573-815-1600</p>
+                  </div>
+               </Col>
+            </Row>
+          </Container>
        </Layout>
    )
 }
